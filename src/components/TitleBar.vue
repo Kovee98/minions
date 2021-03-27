361761873a283b270f9f75205a3a50f3d4abc264
Joshua Kovalchik <kovee98@gmail.com>
2021-03-27 12:38:47 -0500

    feat(logo): add basic logo

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,18 +1,24 @@
 <template>
     <nav class="navbar bg-primary">
         <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Minions</span>
+            <span class="navbar-brand mb-0 h1">
+                <img :src="logo" height="24" class="d-inline-block align-text-top me-1">
+                Minions
+            </span>
         </div>
     </nav>
 </template>
 
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import logo from '../assets/logo.svg';
 
     export default defineComponent({
         name: 'TitleBar',
         setup () {
-            return {};
+            return {
+                logo
+            };
         }
     });
 </script>
